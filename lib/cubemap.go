1803aaf4b988cfbe1a7db0b90f35680c4ccf50d9
Adolfo Mansilla <amansilla@interactive-3d.com>
2016-10-27 19:05:38 -0300

    Se crearon los metodos de generar cubo

--- lib/cubemap.go
+++ lib/cubemap.go
@@ -1,13 +1,25 @@
 package lib
 
-import "math"
+import (
+	"errors"
+	"image/color"
+	"math"
 
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+const (
+	pi = 3.14159265358979323846264338327950288
+)
+
+//23ULFRBD
 //Cubemap save cubemap data
 type Cubemap struct {
-	Ratio    Vector2
-	TileSize Vector2
-	TileMap  [][]int
-	FaceMap  Vector2
+	Ratio          Vector2
+	TileSize       Vector2
+	TileMap        [2][3]string
+	FaceMap        map[string]VectorArray3
+	SquareTileSize int
 }
 
 //Vector2 save vector2 data
@@ -16,21 +28,32 @@ type Vector2 struct {
 	Y int
 }
 
+type LatLong struct {
+	X float64
+	Y float64
+}
+
 //Vector3 save vector3 data
 type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type VectorArray3 struct {
 	X int
 	Y int
-	Z int
+	Z Vector2
 }
 
 //DegreesToRadians convert degrees to radians
 func DegreesToRadians(deg float64) float64 {
-	return deg * math.Pi / 180
+	return deg * pi / 180
 }
 
 //RadiansToDegrees convert radian angles to degrees
 func RadiansToDegrees(rad float64) float64 {
-	return rad * 180.0 / math.Pi
+	return rad * 180.0 / pi
 }
 
 //	LatLonToView
@@ -51,12 +74,135 @@ func VectorFromCoordsRad($latlon)
     return $result;
 }*/
 
+//NewCubemap creates a new Cubemap
+func NewCubemap() (*Cubemap, error) {
+	var layout = [6]string{"U", "L", "F", "R", "B", "D"}
+
+	with := 2
+	height := 3
+
+	c := new(Cubemap)
+	c.FaceMap = make(map[string]VectorArray3)
+
+	ratio, squareTilesize, err := c.GetRatio(with, height)
+	if err != nil {
+		return c, err
+	}
+
+	c.Ratio = ratio
+
+	c.TileSize = Vector2{squareTilesize, squareTilesize}
+
+	//	see if layout fits
+	ratioTileCount := c.Ratio.X * c.Ratio.Y
+	layoutTileCount := len(layout)
+
+	if ratioTileCount != layoutTileCount {
+		//	scale if posss
+		remainder := ratioTileCount % layoutTileCount
+		if remainder != 0 {
+			return c, errors.New("layout: Layout doesn't fit in ratio")
+		}
+		//	ratio can only scale upwards...
+		if ratioTileCount > layoutTileCount {
+			return c, errors.New("layout: Layout doesn't have enough tiles to fit in ratio")
+		}
+
+		scale := layoutTileCount / ratioTileCount
+		c.Ratio.X *= scale
+		c.Ratio.Y *= scale
+		c.TileSize.X /= scale
+		c.TileSize.Y /= scale
+	}
+
+	//	make up 2d map
+	for x := 0; x < c.GetTileWidth(); x++ {
+		for y := 0; y < c.GetTileHeight(); y++ {
+			i := y*c.GetTileWidth() + x
+			face := layout[i]
+			realFace := c.GetRealFace(face)
+
+			//	gr: turn this whole thing into a matrix!
+			matrix := c.GetFaceMatrix(face)
+
+			c.TileMap[x][y] = face
+			c.FaceMap[realFace] = VectorArray3{x, y, matrix}
+
+		}
+	}
+
+	return c, nil
+}
+
 //Resize a Cubemap
-func (c Cubemap) Resize(width, height int) {
-	c.TileSize.X = width / c.Ratio.X
-	c.TileSize.Y = height / c.Ratio.Y
+func (c Cubemap) Resize(width, height int) (int, int) {
+	w := width / c.Ratio.X
+	h := height / c.Ratio.Y
+	return w, h
+}
+
+//GetRatio get the ratio of an image
+func (c Cubemap) GetRatio(width int, height int) (Vector2, int, error) {
+	var vector = Vector2{}
+	if width <= 0 || height <= 0 {
+		return Vector2{}, 0, errors.New("ratio: with or heigh equal to zero")
+	}
+	//	square
+	if width == height {
+
+		return Vector2{1, 1}, width, nil
+	}
+
+	if width > height {
+		remainder := width % height
+		if remainder == 0 {
+
+			return Vector2{width / height, 1}, height, nil
+		}
+
+		vector.X = width / remainder
+		vector.Y = height / remainder
+		return vector, remainder, nil
+	}
+
+	ratio, squareTilesize, err := c.GetRatio(height, width)
+	if err != nil {
+		return Vector2{}, 0, err
+	}
+
+	return Vector2{ratio.Y, ratio.X}, squareTilesize, nil
+}
+
+//GetFaceMatrix get the face Matrix
+func (c Cubemap) GetFaceMatrix(face string) Vector2 {
+	if face == "Z" {
+		return Vector2{-1, -1}
+	}
+
+	return Vector2{1, 1}
 
 }
+
+//GetFlipFace get the flipped face
+func (c Cubemap) GetFlipFace(face string) string {
+	if face == "B" {
+		return "Z"
+	}
+	return ""
+}
+
+//GetRealFace get the real face
+func (c Cubemap) GetRealFace(face string) string {
+	if face == "Z" {
+		return "B"
+	}
+	return face
+}
+
+/*
+func (c Cubemap) IsValid() bool {
+	return if(math.IsNaN(c.Ratio.X))
+}*/
 
 //GetTileWidth get the tile width
 func (c Cubemap) GetTileWidth() int {
@@ -78,6 +224,146 @@ func (c Cubemap) GetImageHeight() int {
 	return c.Ratio.Y * c.TileSize.Y
 }
 
-func (c Cubemap) GetFaceOffset() {
+func (c Cubemap) getSquareTileSize() int {
+	return c.SquareTileSize
+}
 
+//ScreenToWorld get the screen position
+func (c Cubemap) ScreenToWorld(face string, screenPosX float64, screenPosY float64) Vector3 {
+	//	0..1 -> -1..1
+	screenPosX *= 2.0
+	screenPosY *= 2.0
+	screenPosX -= 1.0
+	screenPosY -= 1.0
+	vector := Vector3{0, 0, 0}
+	switch face {
+	case "L":
+		vector.X = -1
+		vector.Y = -screenPosY
+		vector.Z = screenPosX
+		break
+	case "R":
+		vector.X = 1
+		vector.Y = -screenPosY
+		vector.Z = -screenPosX
+		break
+	case "U":
+		vector.X = -screenPosX
+		vector.Y = 1
+		vector.Z = -screenPosY
+		break
+	case "D":
+		vector.X = -screenPosX
+		vector.Y = -1
+		vector.Z = screenPosY
+		break
+	case "F":
+		vector.X = screenPosX
+		vector.Y = -screenPosY
+		vector.Z = 1
+		break
+	case "B":
+		vector.X = -screenPosX
+		vector.Y = -screenPosY
+		vector.Z = -1
+		break
+	}
+
+	return vector
+}
+
+func (c Cubemap) GetFaceColor(face string) color.RGBA {
+	p := color.RGBA{
+		255, 255, 255, 255,
+	}
+	switch face {
+	case "U":
+		p.R = 255
+		p.G = 0
+		p.B = 0
+		break
+	case "L":
+		p.R = 0
+		p.G = 255
+		p.B = 0
+		break
+	case "F":
+		p.R = 0
+		p.G = 0
+		p.B = 255
+		break
+	case "R":
+		p.R = 255
+		p.G = 255
+		p.B = 0
+		break
+	case "B":
+		p.R = 0
+		p.G = 255
+		p.B = 255
+		break
+	case "D":
+		p.R = 255
+		p.G = 0
+		p.B = 255
+		break
+	default:
+		p.R = 255
+		p.G = 255
+		p.B = 255
+		break
+	}
+	return p
+}
+
+func GetRgb(r, g, b float64) string {
+	c := colorful.Color{r, g, b}
+	return c.Hex()
+}
+
+func ViewToLatLon(view3 Vector3) LatLong {
+	var latLong = LatLong{0, 0}
+
+	x := float64(view3.X)
+	y := float64(view3.Y)
+	z := float64(view3.Z)
+
+	lat := math.Atan2(x, z)
+
+	//	normalize y
+	xz := math.Sqrt((x * x) + (z * z))
+	normy := y / math.Sqrt((y*y)+(xz*xz))
+	lon := math.Asin(normy)
+	//$lon = atan2( $y, $xz );
+
+	//	stretch longitude...
+	lon *= 2.0
+
+	latLong.X = lat
+	latLong.Y = lon
+	return latLong
+}
+
+func GetScreenFromLatLong(lat float64, lon float64, width float64, height float64) LatLong {
+	var screenPos = LatLong{0, 0}
+	//	-pi...pi -> -1...1
+	lat /= pi
+	lon /= pi
+
+	//	-1..1 -> 0..2
+	lat += 1.0
+	lon += 1.0
+
+	//	0..2 -> 0..1
+	lat /= 2.0
+	lon /= 2.0
+
+	lon = 1.0 - lon
+	lat *= width
+	lon *= height
+
+	screenPos.X = lat
+	screenPos.Y = lon
+	//fmt.Printf("lat: ", screenPos.X)
+	return screenPos
 }
--- main.go
+++ main.go
@@ -1,5 +1,125 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"image"
+	"log"
+	"math"
+	"os"
 
+	"image/color"
+	_ "image/jpeg"
+	"image/png"
+
+	"github.com/adolfobushi/equirectangular-to-cubic/lib"
+)
+
+func main() {
+	inputFilename := "//home/adolfo/Descargas/prueba_cubo/site_uploadfilezilla.jpg"
+	/*inputLayout := "equirect"
+	sampleWidth := 5064
+	sampleHeight := 2532
+	sampleTime := 0
+
+	outputFilename := "/home/adolfo/Documentos/panopo/panopo/img/img3_ok.jpg"*/
+
+	outputWidth := 4096
+	outputHeight := 6144
+
+	cubemap, err := lib.NewCubemap()
+	if err != nil {
+		fmt.Printf("cubemap: ", err.Error())
+
+	}
+	fmt.Println("cube: %v", cubemap)
+
+	w, h := cubemap.Resize(outputWidth, outputHeight)
+	cubemap.TileSize.X = w
+	cubemap.TileSize.Y = h
+	reader, err := os.Open(inputFilename)
+	//var im image
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	im, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	EquirectToCubemap(im, *cubemap)
+	//fmt.Fprintln("image: %v", im.Bounds())
+	//	$start = microtime(true);
+}
+
+func EquirectToCubemap(equiImage image.Image, cubemap lib.Cubemap) {
+
+	var inWidth float64 = 5064
+	var inHeight float64 = 2532
+
+	outWidth := cubemap.GetImageWidth()
+	outHeight := cubemap.GetImageHeight()
+
+	cubeImage := image.NewNRGBA(image.Rect(0, 0, outWidth, outHeight))
+
+	//	re-using class objects saves cpu time in massive loops
+	//var viewVector lib.VectorArray3 // := lib.VectorArray3{0, 0, lib.Vector2{0, 0}}
+	//var latLon lib.Vector2          // := lib.Vector2{0, 0}
+	//var sphereImagePos lib.Vector2  //:= lib.Vector2{0, 0}
+
+	//	go through each tile, convert pixel to lat long, then read
+	for face, faceOffset := range cubemap.FaceMap {
+
+		var colour color.RGBA = cubemap.GetFaceColor(face)
+		var x int
+		var y int
+		for fy := 0; fy < cubemap.TileSize.Y; fy++ {
+			for fx := 0; fx < cubemap.TileSize.X; fx++ {
+				var screenPos lib.LatLong
+				x = fx + (faceOffset.X * cubemap.TileSize.X)
+				y = fy + (faceOffset.Y * cubemap.TileSize.Y)
+
+				vx := float64(fx) / float64(cubemap.TileSize.X)
+				vy := float64(fy) / float64(cubemap.TileSize.Y)
+				viewVector := cubemap.ScreenToWorld(face, vx, vy)
+
+				if viewVector.X != 0 {
+
+					latLong := lib.ViewToLatLon(viewVector) //	 0.9s
+
+					screenPos = lib.GetScreenFromLatLong(latLong.X, latLong.Y, inWidth, inHeight) //	0.41
+
+					colour = ReadPixelClamped(equiImage, screenPos.X, screenPos.Y, inWidth, inHeight) //	 1.77
+					//fmt.Println("color: ", colour)
+				}
+
+				cubeImage.Set(x, y, colour)
+			}
+		}
+	}
+
+	f, err := os.OpenFile("rgb.png", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	png.Encode(f, cubeImage)
+	//equiImage = cubeImage
+}
+
+//	on 512x512 providing w/h saved 0.2sec
+func ReadPixelClamped(img image.Image, x float64, y float64, w float64, h float64) color.RGBA {
+	colour := color.RGBA{255, 255, 255, 255}
+
+	lat := int(math.Max(0, math.Min(x, w-1)))
+	long := int(math.Max(0, math.Min(y, h-1)))
+	//fmt.Printf("lat: ", lat, long)
+	r, g, b, a := img.At(lat, long).RGBA()
+
+	colour.R = uint8(r)
+	colour.G = uint8(g)
+	colour.B = uint8(b)
+	colour.A = uint8(a)
+	return colour
 }
